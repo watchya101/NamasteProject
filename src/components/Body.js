c8ec503f1ebe08c39c65b944ef2b0b66858521fd
Watchya101 <watchya101@gmail.com>
2024-01-02 11:41:46 +0530

    added conditional rendering as shimmer effect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,34 @@
 import RestaurantCard from "./RestaurentCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+//import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./shimmer";
 const Body = () => {
 // Local State Variable useState returns array can be also written as,
 // const arr = useState(resList);
 // const [listOfRestaurant, setListOfRestaurant] = arr;
 // same as const listOfRestaurant = arr[0] and const setListOfRestaurant = arr[1];
-    const [listOfRestaurant , setListOfRestaurant] = useState(resList); //here we are using destructureing
+    const [listOfRestaurant , setListOfRestaurant] = useState([]); //here we are using destructureing
 
+    useEffect(() => {
+        fetchData();
+        
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.9981729&lng=72.8274691&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json = await data.json();
+            const resPath = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        console.log(resPath);
+        setListOfRestaurant(resPath);
+    };
+
+    //conditional rendering
+    if(listOfRestaurant.length == 0){
+        return <Shimmer/>;
+    }
 
     return(
             <div className="Body">
---
+++ src/components/shimmer.js
@@ -0,0 +1,16 @@
+const shimmer = () => {
+   return (
+        <div className="shimmer-container">
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div>
+        </div>
+    );
+};
+
+export default shimmer;
